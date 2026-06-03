7b02f1c3fd56d9affaa33cd7d48019d1718448b3
Ajaz Mohemmed <ajazmohemmeds@gmail.com>
2026-06-03 18:14:21 +0530

    Use environment variable for database URL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,20 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./lead_qualification.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./lead_qualification.db")
 
 # Using connect_args={"check_same_thread": False} because SQLite is used,
 # allowing multiple threads to access it within FastAPI
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
+        db = SessionLocal()
+        try:
+                    yield db
+finally:
         db.close()
